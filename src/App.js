b439e8b158df736896882b9ff30b1740bace1cbd
hadeel salah <hadeelsalah80@gmail.com>
2020-08-02 21:44:53 +0300

    Add signUpIn page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Home from './Pages/Home/Home.component';
 import Shop from './Pages/Shop/Shop.component'
 import Header from './Layout/Header.component';
+import SignInUp from './Pages/SignUp-In/SignIn-Up.component';
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
         <Switch>
         <Route exact path='/' component={Home} />
         <Route path='/shop' component={Shop} />
+        <Route path='/signin' component={SignInUp} />
+
       </Switch>
       </BrowserRouter>
   );
