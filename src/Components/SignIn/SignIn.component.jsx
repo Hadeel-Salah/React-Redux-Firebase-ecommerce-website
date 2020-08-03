9327463aa348bb0af09e9a97ddc57ed9c289ef1a
hadeel salah <hadeelsalah80@gmail.com>
2020-08-03 20:28:46 +0300

    warrp buttons in div and pass isGoogle sign in

--- src/Components/SignIn/SignIn.component.jsx
+++ src/Components/SignIn/SignIn.component.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import FormInput from './form-input/FormInput.component';
 import CustomButton from './custom-button/custom-button.component';
 import './sign-in.styles.scss';
+import { signInWithGoogle } from '../../firebase/firebase.utils';
+
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -48,7 +50,12 @@ class SignIn extends React.Component {
             label='password'
             required
           />
-          <CustomButton type='submit'> Sign in </CustomButton>
+         <div className="buttons">
+         <CustomButton type='submit'> Sign in </CustomButton>
+          <CustomButton onClick={signInWithGoogle} isGoogleSignIn >
+              Sign in with Google
+            </CustomButton>
+         </div>
         </form>
       </div>
     );
