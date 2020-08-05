956c3046ff1ebf5dd55f312837dc5fd46206e735
hadeel salah <hadeelsalah80@gmail.com>
2020-08-05 20:52:02 +0300

    Add sign Up cpmponent to the page

--- src/Pages/SignUp-In/SignIn-Up.component.jsx
+++ src/Pages/SignUp-In/SignIn-Up.component.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import SignIn from '../../Components/SignIn/SignIn.component';
+import SignUp from '../../Components/SignUp/sign-up.component';
+import './SignIn-Up.styles.scss';
+
+
 
 
 const SignInUp = () => (
   <div className='sign-in-and-sign-up'>
     <SignIn />
+    <SignUp />
   </div>
 );
 
