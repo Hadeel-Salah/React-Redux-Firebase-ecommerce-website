0f6fdb500c507a9c2594c0890dddbfaeca680506
hadeel salah <hadeelsalah80@gmail.com>
2020-08-05 16:04:28 +0300

    store user data in app and firbase

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './Pages/Home/Home.component';
 import Shop from './Pages/Shop/Shop.component'
 import Header from './Layout/Header.component';
 import SignInUp from './Pages/SignUp-In/SignIn-Up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -19,10 +19,23 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+
+          console.log(this.state);
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
     });
   }
 
