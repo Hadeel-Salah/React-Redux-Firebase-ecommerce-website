00139ff6065e59f5b4ca3b95c7470069e04890dc
hadeel salah <hadeelsalah80@gmail.com>
2020-08-29 01:11:02 +0300

    add strip

--- src/Pages/checkout/checkout.component.jsx
+++ src/Pages/checkout/checkout.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+import StripeCheckoutButton from '../../Components/stripe-button/strip-button.component';
 import CheckoutItem from '../../Components/checkout.item/checkout-item'
 import {
   selectCartItems,
@@ -31,6 +32,12 @@ const CheckoutPage = ({ cartItems, total }) => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
     <div className='total'>TOTAL: ${total}</div>
+    <div className='test-warning'>
+      *Please use the following test credit card for payments*
+      <br />
+      4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
