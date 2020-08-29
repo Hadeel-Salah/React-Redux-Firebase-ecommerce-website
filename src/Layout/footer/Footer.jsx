97d33c9f50ea357798e6c45ad0d0eefdacd90568
hadeel salah <hadeelsalah80@gmail.com>
2020-08-29 15:30:23 +0300

    Create Footer.jsx

---
+++ src/Layout/footer/Footer.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+
+function Footer(){
+    return(
+        <footer class="page-footer font-small blue pt-3">
+          <div class="text-center text-md-left"  style={{backgroundColor: '#4444', color: 'white'}}>
+        
+            <div class="row">
+        
+              <div class="col-md-6 mt-md-0 mt-3">
+        
+                <h5 class="text-uppercase">Footer Content</h5>
+                <p>Here you can use rows and columns to organize your footer content.</p>
+        
+              </div>
+        
+              <hr class="clearfix w-100 d-md-none pb-3" ></hr>
+        
+              <div class="col-md-3 mb-md-0 mb-3">
+        
+                <h5 class="text-uppercase">Links</h5>
+        
+                <ul class="list-unstyled">
+                  <li>
+                    <a href="#!">Link 1</a>
+                  </li>
+                  <li>
+                    <a href="#!">Link 2</a>
+                  </li>
+                  <li>
+                    <a href="#!">Link 3</a>
+                  </li>
+                  <li>
+                    <a href="#!">Link 4</a>
+                  </li>
+                </ul>
+        
+              </div>            
+              <div class="col-md-3 mb-md-0 mb-3">
+    
+                <h5 class="text-uppercase">Links</h5>
+        
+                <ul class="list-unstyled">
+                  <li>
+                    <a href="#!">Link 1</a>
+                  </li>
+                  <li>
+                    <a href="#!">Link 2</a>
+                  </li>
+                  <li>
+                    <a href="#!">Link 3</a>
+                  </li>
+                  <li>
+                    <a href="#!">Link 4</a>
+                  </li>
+                </ul>
+        
+              </div>
+        
+            </div>
+        
+          </div>
+            <div class="footer-copyright text-center py-3">© 2020 Copyright:
+            <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
+          </div>
+        
+        </footer>
+    )
+}
+export default Footer;
